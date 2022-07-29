ed5db6fcd01e9456941c5ca6ce4517e78c2bd75e
Decoderseeman <99487702+Decoderseeman@users.noreply.github.com>
2022-07-29 14:41:21 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -30,7 +30,7 @@ async def index_files(bot, query):
         return await query.answer('Wait until previous process complete.', show_alert=True)
     msg = query.message
 
-    await query.answer('<b>ᴘʀᴏᴄᴇssɪɴɢ...⏳</b>', show_alert=True)
+    await query.answer('ᴘʀᴏᴄᴇssɪɴɢ...⏳', show_alert=True)
     if int(from_user) not in ADMINS:
         await bot.send_message(int(from_user),
                                f'**ʏᴏᴜʀ sᴜʙᴍɪssɪᴏɴ ғᴏʀ ɪɴᴅᴇxɪɴɢ** {chat} **ʜᴀs ʙᴇᴇɴ ᴀᴄᴄᴇᴘᴛᴇᴅ ʙʏ ᴏᴜʀ ᴍᴏᴅᴇʀᴀᴛᴏʀs ᴀɴᴅ ᴡɪʟʟ ʙᴇ ᴀᴅᴅᴇᴅ sᴏᴏɴ.**',
